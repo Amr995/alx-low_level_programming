dec21ff67c4db3251bb344b267c1c1c05b74f346
Amr995 <amr995@yahoo.com>
2024-01-10 15:20:44 -0800

    print_rev_programming

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _pow_recursion - print the value of x raised to the power of y
- * @x: first number
- * @y: second number
+ * _pow_recursion - search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
  * return: the power of numbers.
  */
 
 int _pow_recursion(int x, int y)
 {
-	if (y <= 0)
+	if (y < 0)
 		return (-1);
 	else if (y == 1)
 		return (x);
+	else if (y == 0)
+		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
