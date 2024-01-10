4043df49939eac4a872cdb7f13d7282153e34330
Amr995 <amr995@yahoo.com>
2024-01-10 14:08:40 -0800

    print_rev_programming

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (s == '\n')
+		_putchar(strlen(s));
+	else
+		_strlen_recursion(s + 1);
+}
