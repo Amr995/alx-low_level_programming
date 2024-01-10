c776dfece0943fcee75f00c333ddc787a7c2dcb5
Amr995 <amr995@yahoo.com>
2024-01-10 15:13:50 -0800

    print_rev_programming

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print the value of x raised to the power of y
+ * @x: first number
+ * @y: second number
+ * return: the power of numbers.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y = 1)
+		return (x);
+	else
+		return (x *_pow_recursion(x, y - 1));
+}		       
