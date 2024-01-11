36971eec28687ee46ab758ff644918b2b2efa73a
Amr995 <amr995@yahoo.com>
2024-01-11 10:50:46 -0800

    is prime number

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,7 @@
+int is_palindrome(char *s)
+{
+	short st = 0;
+	short ed = strlen(s);
+	if(s[st] == s[ed])
+	{
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - check if n is a prime number
+ * @other: int
+ * @n: int
+ * Return: 1(success), 0(failure)
+ */
+
+int check_prime(int n, int other);
+int is_prime_number(int n)
+{
+	return (check_prime(n,2));
+}
+
+/**
+ * check_prime - check all number < n if they can divide it
+ * @n: int
+ * @other: int
+ * Return: int
+ */
+
+int check_prime(int n, int other)
+{
+	if (other >= n && n > 1)
+		return (1);
+	else if (n % other == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, other + 1));
+}
