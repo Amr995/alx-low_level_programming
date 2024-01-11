91529cbed1e6ab0f95302600869dd885bdc884e4
Amr995 <amr995@yahoo.com>
2024-01-10 16:03:10 -0800

    print_rev_programming

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,24 @@
 #include "main.h"
 /**
- * _sqrt_recursion: Print the result of square n.
+ * _sqrt_recursion - print the result of square n.
  * @n: int
  * @val: squar root
  * Return: The result
  */
+
 int square(int n, int val);
 int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
 }
 
-/** square - find square root
+/**
+ * square - find square root
  * @n: int to find square root
  * @val: squar root
  * Return: The result
  */
+
 int square(int n, int val)
 {
 	if (val * val == n)
