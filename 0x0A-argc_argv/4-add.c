bd3764cc59a2233ebc4136400f85817f3f93ff12
Amr995 <amr995@yahoo.com>
2024-01-17 10:57:11 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,14 +11,15 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	char *c;
 
-	while (--argc)
+	for (int i = 1; i < argc; i++)
 	{
-		for (c = argv[argc]; *c; c++)
-			if (*c < '0' || *c > '9')
-				return (printf("Error\n", 1));
-			sum += atoi(arhv[argc]);					}
+		int x = atoi(argv[i]);
+
+		if (x < 0 || x > 9)
+			return (printf("Error\n"), 1);
+		sum += x;
+	}
 	printf("%d\n", sum);
 	return (0);
 }
