dc360c7f45a3c5f260e019d3de199d264769b5ab
Amr995 <amr995@yahoo.com>
2024-01-17 10:41:06 -0800

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int 1, leastcents = 0, money = atoi(argv[1]);
+		int i, leastcents = 0, money = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
 
 		for (i = 0; i < 5; i++)
 		{
 			if (money >= cents[i])
 			{
-				leasetcents += money / cents[i];
+				leastcents += money / cents[i];
 				money = money % cents[i];
 				if (money % cents[i] == 0)
 				{
