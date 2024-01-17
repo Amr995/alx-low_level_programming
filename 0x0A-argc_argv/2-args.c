774ef4bedd856df0d5511d8ada291b096921f6cf
Amr995 <amr995@yahoo.com>
2024-01-17 09:41:14 -0800

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,8 +9,8 @@ int main(int argc, char const *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);	
+
 	return (0);
 }
