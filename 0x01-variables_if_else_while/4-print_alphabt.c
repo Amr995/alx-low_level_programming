f3452e1e2c91a440cd86e55f0b7b932498abce9a
Amr995 <amr995@yahoo.com>
2024-04-23 14:24:41 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ * Description: function that print alphabet in lowercase
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i = 'a', I = 'A';
+
+	while (i <= 'z')
+	{
+		if (i != 'q' || i != 'e')
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
