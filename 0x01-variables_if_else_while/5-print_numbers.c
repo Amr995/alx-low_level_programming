3d9c16d51d46cd8bf485615a1354fabb214406ef
Amr995 <amr995@yahoo.com>
2024-04-23 14:38:21 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ * Description: function that print number from 0 to 9
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= i)
+	{
+		put(i);
+		i++;
+	}
+	put('\n');
+
+	return (0);
+}
